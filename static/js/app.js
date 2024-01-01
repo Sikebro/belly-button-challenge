035d13b995d462c5939628f136e6a44ca85579f2
shils <shaon@usf.edu>
2023-12-31 20:48:08 -0500

    started

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+
+
+// Using D3 library to read in samples.json
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// Horizontal Bar chart displaying the top 10 OTUs
+
